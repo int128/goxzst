03a4ac259c66984897e46c37fafcc4d462f58209
Hidetake Iwata <int128@gmail.com>
2019-04-04 10:33:44 +0900

    Refactor

--- models/build/build.go
+++ models/build/build.go
@@ -1,9 +1,15 @@
 package build
 
+import "fmt"
+
 type GOOS string
 type GOARCH string
 
 type Platform struct {
 	GOOS   GOOS
 	GOARCH GOARCH
+}
+
+func (p Platform) String() string {
+	return fmt.Sprintf("%s_%s", p.GOOS, p.GOARCH)
 }
--- usecases/make.go
+++ usecases/make.go
@@ -5,6 +5,7 @@ import (
 	"path/filepath"
 
 	"github.com/int128/goxzst/adaptors/interfaces"
+	"github.com/int128/goxzst/models/build"
 	"github.com/int128/goxzst/usecases/interfaces"
 	"github.com/pkg/errors"
 	"go.uber.org/dig"
@@ -27,47 +28,14 @@ type Make struct {
 func (u *Make) Do(in usecases.MakeIn) error {
 	var executableFilenames []string
 	templateVariables := make(map[string]string)
+
 	for _, platform := range in.Platforms {
-		executableFilename := filepath.Join(in.OutputDir,
-			fmt.Sprintf("%s_%s_%s", in.OutputName, platform.GOOS, platform.GOARCH))
-		zipFilename := executableFilename + ".zip"
-		shaFilename := executableFilename + ".zip.sha256"
-
-		if err := u.CrossBuild.Do(usecases.CrossBuildIn{
-			OutputFilename: executableFilename,
-			GoBuildArgs:    in.GoBuildArgs,
-			Platform:       platform,
-		}); err != nil {
-			return errors.Wrapf(err, "error while cross build")
-		}
-
-		archiveEntries := []usecases.ArchiveEntry{{
-			Path:          in.OutputName,
-			InputFilename: executableFilename,
-		}}
-		for _, zipExtraFilename := range in.ArchiveExtraFilenames {
-			archiveEntries = append(archiveEntries, usecases.ArchiveEntry{
-				Path:          zipExtraFilename,
-				InputFilename: zipExtraFilename,
-			})
-		}
-		if err := u.Archive.Do(usecases.ArchiveIn{
-			OutputFilename: zipFilename,
-			Entries:        archiveEntries,
-		}); err != nil {
-			return errors.Wrapf(err, "error while creating zip")
-		}
-
-		out, err := u.Digest.Do(usecases.DigestIn{
-			InputFilename:  zipFilename,
-			OutputFilename: shaFilename,
-		})
+		out, err := u.build(in, platform)
 		if err != nil {
-			return errors.Wrapf(err, "error while creating digest")
+			return errors.Wrapf(err, "error while build for the platform %s", platform)
 		}
-
-		executableFilenames = append(executableFilenames, executableFilename)
-		templateVariables[fmt.Sprintf("%s_%s_zip_sha256", platform.GOOS, platform.GOARCH)] = out.SHA256
+		executableFilenames = append(executableFilenames, out.executableFilename)
+		templateVariables[fmt.Sprintf("%s_%s_zip_sha256", platform.GOOS, platform.GOARCH)] = out.digestOut.SHA256
 	}
 
 	for _, t := range in.TemplateFilenames {
@@ -87,4 +55,51 @@ func (u *Make) Do(in usecases.MakeIn) error {
 		}
 	}
 	return nil
+}
+
+type buildOut struct {
+	executableFilename string
+	digestOut          *usecases.DigestOut
+}
+
+func (u *Make) build(in usecases.MakeIn, platform build.Platform) (*buildOut, error) {
+	basename := filepath.Join(in.OutputDir, fmt.Sprintf("%s_%s_%s", in.OutputName, platform.GOOS, platform.GOARCH))
+	executableFilename := basename
+	zipFilename := executableFilename + ".zip"
+	shaFilename := executableFilename + ".zip.sha256"
+
+	if err := u.CrossBuild.Do(usecases.CrossBuildIn{
+		OutputFilename: executableFilename,
+		GoBuildArgs:    in.GoBuildArgs,
+		Platform:       platform,
+	}); err != nil {
+		return nil, errors.Wrapf(err, "error while cross build")
+	}
+
+	archiveEntries := []usecases.ArchiveEntry{{
+		Path:          in.OutputName,
+		InputFilename: executableFilename,
+	}}
+	for _, f := range in.ArchiveExtraFilenames {
+		archiveEntries = append(archiveEntries, usecases.ArchiveEntry{Path: f, InputFilename: f})
+	}
+	if err := u.Archive.Do(usecases.ArchiveIn{
+		OutputFilename: zipFilename,
+		Entries:        archiveEntries,
+	}); err != nil {
+		return nil, errors.Wrapf(err, "error while creating zip")
+	}
+
+	digestOut, err := u.Digest.Do(usecases.DigestIn{
+		InputFilename:  zipFilename,
+		OutputFilename: shaFilename,
+	})
+	if err != nil {
+		return nil, errors.Wrapf(err, "error while creating digest")
+	}
+
+	return &buildOut{
+		executableFilename: executableFilename,
+		digestOut:          digestOut,
+	}, nil
 }
